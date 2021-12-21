9bdc2f2ed4a4e04c4a2749bf09a0aa4899b966be
AGPA-88 <agpa88@gmail.com>
2021-12-21 11:45:12 +0100

    Error verification fixed Validation updated

--- starterOnly/app.js
+++ starterOnly/app.js
@@ -3,33 +3,86 @@ const inputs = document.querySelectorAll("#reserve input");
 
 
 let formData = {};
+let errors = {}
 
 const handleChange = (e) => {
 
     console.log({[e.target.name]: e.target.value});
     formData = {...formData, [e.target.name]: e.target.value};
+    if (e.terget.type === "checkbox") {
+        formData[e.target.name] = e.target.checked;
+    } else {
+        formData[e.target.name] = e.target.value;
+    }
 
 }
 
-inputs.forEach(input => input.addEventListener('change', handleChange))
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+
+    validate();
+
+    if(Object.keys(errors).length > 0) return;
+
+    
+    
+    reserveForm.parentElement.innerHTML = `
+        <div class="success">
+            <h2>Thank you! Your reservation was received.</h2>
+        </div>
+    `
+    
+
+}
+
+inputs.forEach(input => {
+    input.addEventListener('change', handleChange)
+    input.addEventListener('keyup', handleChange)
+})
 
 
-const validate = (e)=> {
+const validate = () => {
 
-    const errors = {}
+    document.querySelectorAll('input.has-error').forEach(input => {
+        if(Object.keys(errors).includes(input.name)) {
+            if(input.name === 'location' || input.name === 'toc') {
+                document.querySelector(`input[name="${input.name}"]`).classList.remove('has-error');
+                document.querySelector(`input[name="${input.name}"] + label + p`).innerText = ''
+            }else{
+                document.querySelector(`#${input.name}`).classList.remove('has-error');
+                document.querySelector(`#${input.name} + p`).innerText = ''
+            }
+
+        }
+
+        delete errors[input.name];
+
+    })
 
     if(formData.first?.length < 2)  errors.first = "First name must have more than 2 characters."
     if(!formData.first) errors.first = "First name field cannot be blank."
     if(formData.last?.length < 2)  errors.last = "Last name must have more than 2 characters."
     if(!formData.last) errors.last = "Last name field cannot be blank."
-    // if(formData.email?.length < 2)  errors.email = "You have to enter a valid email"
+    if(!ValidateEmail(formData.email))  errors.email = "You have to enter a valid email"
     if(!formData.email) errors.email = "Email field cannot be blank."
+    if(!formData.birthdate) errors.birthdate = "You must enter your date of birth."
+    if(isNaN(formData.quantity)) errors.quantity = "You must enter a valid number."
+    if(!formData.location) errors.location = "You must choose a location."
+    if(!formData.toc) errors.toc = "You must accept our terms."
 
 
-    if(Object.keys(errors).length > 0) e.preventDefault();
 
     Object.keys(errors).forEach(error => {
-        document.querySelector(`#${error} + p`).innerText = errors[error]
+
+        if(error === 'location' || error === 'toc') {
+            document.querySelector(`input[name="${error}"]`).classList.add('has-error');
+            document.querySelector(`input[name="${error}"] + label + p`).innerText = errors[error] 
+        }else{
+            document.querySelector(`#${error}`).classList.add('has-error');
+            document.querySelector(`#${error} + p`).innerText = errors[error]
+        }
+
     })
 
 
@@ -38,4 +91,12 @@ const validate = (e)=> {
 
 };
 
-reserveForm.addEventListener("submit", validate);
+
+
+function ValidateEmail(mail) {
+    
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(mail);
+}
+
+reserveForm.addEventListener("submit", handleSubmit);
--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,4 +1,4 @@
-function editNav() {
+    function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
